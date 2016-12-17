928793b390df0ec53a6fcf69f7266b935001623b
cmd-kvn <wong.kevin@student.clackamas.edu>
2016-12-17 00:56:21 -0800

    'started over' added data-author attr to the clone, not the element where author name was located checked populateFilters was functional finished handleAuthorFilter

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -21,18 +21,17 @@ articleView.populateFilters = function() {
 articleView.handleAuthorFilter = function() {
   $('#author-filter').on('change', function() {
   if ($(this).val()) {
+    var $article = $('article');
+    var authorName = $(this).val();
     /* TODO: If the slect box changes to an option that has a value, we should:*/
     // 1. Hide all of the articles
-    var $article = $('article');
     $article.hide();
     // 2. Fade in only the articles that match based on on the author that was aselected. Hint: use an attribute selector to find those articles that match the value, and then fade them in.
-    //event listener
-    var $authorFilter = $('#author-filter');
-    var authorMenu = $authorFilter.find('option').val();
-    var authorArticle = $article.find('.byline a').attr();
+    $('article[data-author="' + authorName + '"]').fadeIn(2000);
   } else {
     /* Otherwise, we should:
         1. Show all the articles except the template */
+    $article.not('template').show();
   }
     $('#category-filter').val('');
   });
